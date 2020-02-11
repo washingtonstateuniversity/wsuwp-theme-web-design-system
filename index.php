eda153ac20aba82ec52ccdc8ae72e0d930af7d04
danbleile <danbleile@gmail.com>
2020-02-11 08:14:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 		<?php get_template_part( 'template-parts/site-nav-horizontal', get_post_type() ); ?>
 		<div class="wsu-s-main-container">
 			<div class="wsu-c-container">
+			<?php get_template_part( 'template-parts/site-hero-banner-overlay', get_post_type() ); ?>
+			<?php get_template_part( 'demo/demo-content-columns', get_post_type() ); ?>
 			<?php if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post();
