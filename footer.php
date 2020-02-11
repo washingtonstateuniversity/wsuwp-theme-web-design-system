34bb76ed4c8e16a86bdbb1bbb00e7bb026c13152
danbleile <danbleile@gmail.com>
2020-02-11 15:06:40 -0800

    Add basic form styling

--- footer.php
+++ footer.php
@@ -5,6 +5,21 @@ $demo_options = get_theme_mod( 'wsu_wsd_demo', array() );
 ?>
 <?php if ( ! empty( $demo_options['is_demo'] ) ) : ?>
 <script type='text/javascript' src='https://cdn.web.wsu.edu/designsystem/1.x/build/dist/wsu-design-system.bundle.dist.js?ver=<?php echo esc_attr( WSUWP\Theme\Binder\Theme::get_version() ); ?>'></script>
+<style>
+	.gform_wrapper {
+		border-top: 1px solid rgba(0,0,0,0.25);
+		padding-top: 20px;
+		margin-top: 20px;
+		padding-bottom: 20px;
+	}
+	.gform_body ul {
+		list-style-type: none;
+		padding-inline-start: 0 !important;
+	}
+	.gfield {
+		margin-bottom: 1rem;
+	}
+</style>
 <?php else : ?>
 	<?php wp_footer(); ?>
 <?php endif; ?>
--- includes/include-theme.php
+++ includes/include-theme.php
@@ -4,7 +4,7 @@
 class Theme {
 
 
-	protected static $version = '0.0.2.6';
+	protected static $version = '0.0.2.7';
 
 
 	public function init() {
