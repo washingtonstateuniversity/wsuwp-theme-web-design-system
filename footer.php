ccf88129985dd17d42489ff687e2c776f945bdca
danbleile <danbleile@gmail.com>
2020-02-11 15:07:05 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@ $demo_options = get_theme_mod( 'wsu_wsd_demo', array() );
 <?php if ( ! empty( $demo_options['is_demo'] ) ) : ?>
 <script type='text/javascript' src='https://cdn.web.wsu.edu/designsystem/1.x/build/dist/wsu-design-system.bundle.dist.js?ver=<?php echo esc_attr( WSUWP\Theme\Binder\Theme::get_version() ); ?>'></script>
 <style>
+	/* For demo only */
 	.gform_wrapper {
 		border-top: 1px solid rgba(0,0,0,0.25);
 		padding-top: 20px;
