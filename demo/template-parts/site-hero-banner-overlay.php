c8641a11462443867daefe0704a3eb95178c8d32
danbleile <danbleile@gmail.com>
2020-02-19 08:48:11 -0800

    Hero Component / Fix styling

--- demo/template-parts/site-hero-banner-overlay.php
+++ demo/template-parts/site-hero-banner-overlay.php
@@ -1,5 +1,5 @@
 <!-- COMPONENT:START -->
-<div class="wsu-c-hero__wrapper wsu-u-no-js">
+<div class="wsu-c-hero__wrapper--full-width wsu-u-no-js">
 	<div class="wsu-c-hero__container" style="background-image: url('https://source.unsplash.com/random');">
 		<div class="wsu-c-hero__content">
 			<h1 class="wsu-c-hero__title">Odio et purus metus placerat neque.</h1>
@@ -10,38 +10,3 @@
 	</div>
 </div>
 <!-- COMPONENT:END -->
-<style>
-	.wsu-c-hero__wrapper {
-		margin-right: calc( ( ( ( 100vw - 100% - 65px ) / 2 ) ) * -1 );
-		margin-left: calc( ( ( ( 100vw - 100% - 65px ) / 2 ) ) * -1 );
-		-webkit-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-        -o-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-        transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-	}
-
-	/*.wsu-s-nav-vertical__wrapper--open ~ .wsu-s-wrapper .wsu-s-main-container .wsu-c-container .wsu-c-hero__wrapper {
-		margin-right: calc( ( ( ( 100vw - 100% - 365px ) / 2 )  ) * -1 );
-		margin-left: calc( ( ( ( 100vw - 100%  - 365px ) / 2 ) ) * -1 );
-		-webkit-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-        -o-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-        transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-	}*/
-
-	@media only screen and (min-width: 1200px) {
-		.wsu-s-nav-vertical__wrapper--open ~ .wsu-s-wrapper .wsu-s-main-container .wsu-c-container .wsu-c-hero__wrapper {
-			margin-right: calc( ( ( ( 100vw - 100% - 365px ) / 2 )  ) * -1 );
-			margin-left: calc( ( ( ( 100vw - 100%  - 365px ) / 2 ) ) * -1 );
-			-webkit-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-			-o-transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-			transition: all 300ms cubic-bezier(0, 0, 0.03, 1);
-		} 
-	}
-
-	@media only screen and (max-width: 767px) {
-		.wsu-c-hero__wrapper,
-		.wsu-s-nav-vertical__wrapper--open ~ .wsu-s-wrapper .wsu-s-main-container .wsu-c-container .wsu-c-hero__wrapper {
-			margin-right: 0;
-			margin-left: 0;
-		} 
-	}
-</style>
