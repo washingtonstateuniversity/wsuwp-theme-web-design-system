52910050670c456dc28eba6a09d0635b8fab560a
Danial Bleile <danbleile@gmail.com>
2020-03-25 06:09:19 -0700

    Add wsu-c-content wrapper

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<?php get_template_part( 'template-parts/site-header', get_post_type() ); ?>
 		<?php get_template_part( 'template-parts/site-nav-horizontal', get_post_type() ); ?>
 		<div class="wsu-s-main-container">
-			<div class="wsu-c-container">
+			<div class="wsu-c-container wsu-c-content">
 			<?php get_template_part( 'template-parts/site-hero-banner-overlay', get_post_type() ); ?>
 			<?php get_template_part( 'demo/demo-content-columns', get_post_type() ); ?>
 			<?php if ( have_posts() ) {
