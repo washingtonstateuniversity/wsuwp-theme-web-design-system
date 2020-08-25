b1509cfbfd6bd0911bbf36c7661c8ec109376611
Danial Bleile <danbleile@gmail.com>
2020-08-25 07:25:28 -0700

    Add tabindex for keyboard support

--- packages/blocks/site-nav-vertical/templates/default.php
+++ packages/blocks/site-nav-vertical/templates/default.php
@@ -49,7 +49,7 @@
 			<?php endforeach; ?>
 		</ul>
 	</nav>
-	<a class="wsu-s-nav-vertical__menu-icon-link" href="#" aria-haspopup="true" aria-expanded="true">
+	<a class="wsu-s-nav-vertical__menu-icon-link" href="#" aria-haspopup="true" aria-expanded="true" tabindex="0">
 		<div class="wsu-s-nav-vertical__menu-icon-container">
 			<div class="wsu-s-nav-vertical__menu-icon-bar"></div>
 			<div class="wsu-s-nav-vertical__menu-icon-bar"></div>
