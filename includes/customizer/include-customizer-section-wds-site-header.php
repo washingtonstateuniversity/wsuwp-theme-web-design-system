19ca34c651f9c10ac919220354fa5be2a503270e
Danial Bleile <danbleile@gmail.com>
2020-12-10 11:58:10 -0800

    Enhancement | Filter title tag to include WSU

--- includes/customizer/include-customizer-section-wds-site-header.php
+++ includes/customizer/include-customizer-section-wds-site-header.php
@@ -43,11 +43,29 @@ class Customizer_Section_Site_Header {
 			)
 		);
 
+		$wp_customize->add_setting(
+			'wsu_wds_site_header_on_home',
+			array(
+				'default'     => true,
+				'transport'   => 'refresh',
+			)
+		);
+
 		$wp_customize->add_control(
 			'wsu_wds_site_header_is_active_control',
 			array(
 				'settings' => 'wsu_wds_site_header_is_active',
 				'label'    => 'Show Site Header',
+				'section'  => self::get( 'section_id' ),
+				'type'     => 'checkbox',
+			)
+		);
+
+		$wp_customize->add_control(
+			'wsu_wds_site_header_on_home_control',
+			array(
+				'settings' => 'wsu_wds_site_header_on_home',
+				'label'    => 'Show Site Header On Homepage',
 				'section'  => self::get( 'section_id' ),
 				'type'     => 'checkbox',
 			)
