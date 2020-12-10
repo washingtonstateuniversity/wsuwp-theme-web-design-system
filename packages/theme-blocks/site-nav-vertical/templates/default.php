9f1383087e2a0980ba908cca6e9c110dc9a9bbff
Danial Bleile <danbleile@gmail.com>
2020-12-10 11:57:32 -0800

    Enhancement | Allow remove site header on homepage

--- packages/theme-blocks/site-nav-vertical/templates/default.php
+++ packages/theme-blocks/site-nav-vertical/templates/default.php
@@ -15,33 +15,33 @@
 		</a>
 		<ul class="wsu-s-nav-vertical__nav-list-container" id="wsu-s-nav-vertical" role="menubar" aria-label="Vertical Site Navigation">
 			<?php foreach ( $menu_items as $first_level_item ) : ?>
-			<li class="wsu-s-nav-vertical__nav-item<?php if ( ! empty( $first_level_item['children'] ) ) : ?>--has-children<?php endif; ?>" role="none">
+			<li class="wsu-s-nav-vertical__nav-item<?php if ( ! empty( $first_level_item['children'] ) ) : ?>--has-children<?php endif; ?>" >
 				<a 
 					href="<?php echo esc_url( $first_level_item['url'] ); ?>" 
 					class="wsu-s-nav-vertical__nav-link<?php if ( $first_level_item['is_current_item'] ) : ?>--active<?php endif; ?>"
-					<?php if ( $first_level_item['is_current_item'] || $first_level_item['is_current_ancestor'] ) : ?>tabindex="-1" aria-expanded="true"<?php endif; ?>
+					<?php if ( $first_level_item['is_current_item'] || $first_level_item['is_current_ancestor'] ) : ?> aria-expanded="true"<?php endif; ?>
 					role="menuitem">
 					<?php echo esc_html( $first_level_item['title'] ); ?>
 				</a>
 				<?php if ( ! empty( $first_level_item['children'] ) ) : ?>
 				<ul class="wsu-s-nav-vertical__nav-list" role="menu" >
 					<?php foreach ( $first_level_item['children'] as $second_level_item ) : ?>
-					<li class="wsu-s-nav-vertical__nav-item<?php if ( ! empty( $second_level_item['children'] ) ) : ?>--has-children<?php endif; ?>" role="none">
+					<li class="wsu-s-nav-vertical__nav-item<?php if ( ! empty( $second_level_item['children'] ) ) : ?>--has-children<?php endif; ?>" >
 						<a 
 							href="<?php echo esc_url( $second_level_item['url'] ); ?>" 
 							class="wsu-s-nav-vertical__nav-link<?php if ( $second_level_item['is_current_item'] ) : ?>--active<?php endif; ?>"
-							<?php if ( $second_level_item['is_current_item'] || $second_level_item['is_current_ancestor'] ) : ?>tabindex="-1" aria-expanded="true"<?php endif; ?>
+							<?php if ( $second_level_item['is_current_item'] || $second_level_item['is_current_ancestor'] ) : ?> aria-expanded="true"<?php endif; ?>
 							>
 							<?php echo esc_html( $second_level_item['title'] ); ?>
 						</a>
 						<?php if ( ! empty( $second_level_item['children'] ) ) : ?>
 						<ul class="wsu-s-nav-vertical__nav-list" role="menu" >
 							<?php foreach ( $second_level_item['children'] as $third_level_item ) : ?>
-							<li class="wsu-s-nav-vertical__nav-item" role="none">
+							<li class="wsu-s-nav-vertical__nav-item" >
 								<a 
 									href="<?php echo esc_url( $third_level_item['url'] ); ?>" 
 									class="wsu-s-nav-vertical__nav-link<?php if ( $third_level_item['is_current_item'] ) : ?>--active<?php endif; ?>"
-									<?php if ( $third_level_item['is_current_item'] || $third_level_item['is_current_ancestor'] ) : ?>tabindex="-1" aria-expanded="true"<?php endif; ?>
+									<?php if ( $third_level_item['is_current_item'] || $third_level_item['is_current_ancestor'] ) : ?> aria-expanded="true"<?php endif; ?>
 									>
 									<?php echo esc_html( $third_level_item['title'] ); ?>
 								</a>
--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -4,7 +4,7 @@ Block_Site_Header::render_block(
 	array(
 		'title'     => get_bloginfo( 'name' ),
 		'subtitle'  => get_bloginfo( 'description' ),
-		'on_home'   => true,
+		'on_home'   => get_theme_mod( 'wsu_wds_site_header_on_home', true ),
 		'is_active' => get_theme_mod( 'wsu_wds_site_header_is_active', true ),
 	)
 );
